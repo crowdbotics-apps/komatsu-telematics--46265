f71a25e10866bc27e8f644eae90d1a745acf7974
Crowdbotics <team@crowdbotics.com>
2024-02-12 16:12:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "komatsu-telematics--46265",
+  url: "https://komatsu-telematics--46265.botics.co",
+  api: "https://komatsu-telematics--46265.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
